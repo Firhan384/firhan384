a9ebbd3fbf8bd1d64b716603db6f341622d3745f
firhangael <firhan@rajamobil.net>
2020-07-22 02:18:07 +0700

    adding fitur portofolio & contact me

---
+++ assets/js/contact.js
@@ -0,0 +1,10 @@
+var contact = 
+[
+    [
+        'firhangael1525@gmail.com',
+        'https://www.facebook.com/firhangael15',
+        'https://www.linkedin.com/in/firhan-abigael-abb619142/',
+        'https://github.com/Firhan384',
+        '6285772499060'
+    ]
+];
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,17 +4,17 @@ var portfolio = [
     [
         'System Travel',
         'Travel ini dibangun dengan bahasa pemrogrman PHP dibuild dengan framework Codeigniter 3',
-        'contoh.jpg'
+        'project.png'
     ],
     [
         'Perpustakaan Sekolah',
         'Perpustakaan sekolah dibangun dengan bahasa pemrograman PHP dibuild dengan framework codeigniter 4 yang memiliki fitur kirim notice kepada peminjam buku jika hampir sudah mencapai batas waktu peminjamannya dengan whatsapp',
-        'contoh.jpg'
+        'project.png'
     ],
     [
         'CMS Blog',
         'CMS blog yaitu website yang memiliki fungsi sama seperti blogger yang disediakan oleh google, website ini memanfaatkan API google blog & terdapat fitur scrapping sekaligus spinner text',
-        'contoh.jpg'
+        'cms.png'
     ]
 
 ];
