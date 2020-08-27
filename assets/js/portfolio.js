4fbd9b74f3e12a96075f1d2ab9438063bb65b2cf
firhangael <firhan@rajamobil.net>
2020-08-27 15:48:22 +0700

    penambahan portfolio bakoelsayur

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -15,6 +15,11 @@ var portfolio = [
         'CMS Blog',
         'CMS blog yaitu website yang memiliki fungsi sama seperti blogger yang disediakan oleh google, website ini memanfaatkan API google blog & terdapat fitur scrapping sekaligus spinner text',
         'cms.png'
+    ],
+    [
+        'BakoelSayur - Online Shop',
+        'BakoelSayur adalah suatu website yang bergerak dibidang E-commerce, dibangun dengan bahasa pemrograman web PHP dibalut dengan framework Laravel Versi 7 dan telah terintegrasi dengan metode pembayaran online',
+        'bakoelsayur.png'
     ]
 
 ];
