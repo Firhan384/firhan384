b7e94da16e54383aa177bf6162f1948bbaab4eeb
firhan abigael <firhangael1525@gmail.com>
2020-10-16 16:35:09 +0700

    menambahkan project baru

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,25 +1,27 @@
 var portfolio = [
-
-
-    [
-        'System Travel',
-        'Travel ini dibangun dengan bahasa pemrogrman PHP dibuild dengan framework Codeigniter 3',
-        'project.png'
-    ],
-    [
-        'Perpustakaan Sekolah',
-        'Perpustakaan sekolah dibangun dengan bahasa pemrograman PHP dibuild dengan framework codeigniter 4 yang memiliki fitur kirim notice kepada peminjam buku jika hampir sudah mencapai batas waktu peminjamannya dengan whatsapp',
-        'project.png'
-    ],
-    [
-        'CMS Blog',
-        'CMS blog yaitu website yang memiliki fungsi sama seperti blogger yang disediakan oleh google, website ini memanfaatkan API google blog & terdapat fitur scrapping sekaligus spinner text',
-        'cms.png'
-    ],
-    [
-        'BakoelSayur - Online Shop',
-        'BakoelSayur adalah suatu website yang bergerak dibidang E-commerce, dibangun dengan bahasa pemrograman web PHP dibalut dengan framework Laravel Versi 7 dan telah terintegrasi dengan metode pembayaran online',
-        'bakoelsayur.png'
-    ]
-
+  [
+    "System Travel",
+    "Travel ini dibangun dengan bahasa pemrogrman PHP dibuild dengan framework Codeigniter 3",
+    "project.png",
+  ],
+  [
+    "Perpustakaan Sekolah",
+    "Perpustakaan sekolah dibangun dengan bahasa pemrograman PHP dibuild dengan framework codeigniter 4 yang memiliki fitur kirim notice kepada peminjam buku jika hampir sudah mencapai batas waktu peminjamannya dengan whatsapp",
+    "project.png",
+  ],
+  [
+    "CMS Blog",
+    "CMS blog yaitu website yang memiliki fungsi sama seperti blogger yang disediakan oleh google, website ini memanfaatkan API google blog & terdapat fitur scrapping sekaligus spinner text",
+    "cms.png",
+  ],
+  [
+    "BakoelSayur - Online Shop",
+    "BakoelSayur adalah suatu website yang bergerak dibidang E-commerce, dibangun dengan bahasa pemrograman web PHP dibalut dengan framework Laravel Versi 7 dan telah terintegrasi dengan metode pembayaran online",
+    "bakoelsayur.png",
+  ],
+  [
+    "Time Tracking Landing Page",
+    "Time Tracking Landing Page adalah latihan slicing saya dari sebuah website ternama yaitu figma",
+    "latihan1.png",
+  ],
 ];
