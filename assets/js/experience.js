48dfcc61a07afc75b0e2e394fd43b35f8f6eb9a0
Firhan384 <52119314+Firhan384@users.noreply.github.com>
2023-02-23 01:40:12 +0700

    Update experience.js
    update experience

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -4,22 +4,45 @@ var experience = [
         'PT Redbuzz Mediatama',
         '2020 - Present',
         'Fullstack Developer',
-    ],
-
-    [
-        'PT Global Digital Niaga(Blibli.com)',
-        '2015 - 2020',
-        'Leader Fulfillment',
         [
-            'Setting , mereview dan memonitoring KPI setiap hari pada aspek untuk area fulfillment',
-            'menghandle operasional harian untuk area fulfillment dengan rata rata total order 4000 dan sampai dengan 9000 pcs.',
-            'Melakukan Koordinasi dengan pihak logistik 3PL dan memastikan order customer sampai dengan tepat waktu sesuai dengan standard waktu yang ditentukan.',
-            'Membuat Jadwal unuk karyawan',
-            'Mengasawasi 10-20 Karyawan',
-            'Melakukan investigasi terhadap pesanan customer apabila ada indikasi kesalahan',
-            'Memiliki pengetahuan terhadap Warehouse inbound, inventory control dan outbound serta fulfillment.',
-            'Memastikan berjalannya pekerjaan sesuai dengan SOP'
+            'Redbuzz Mediatama is software house located west jakarta.',
+            'My Responsibility :',
+            '- Developing restfull api using Nodejs with typescript, mobile apps using React Native, frontend using Angular and Reactjs.',
+            '- Responsible for development software to work on, support and maintain.',
+            '- Responsible for improving, adding features to the app if needed.',
+            '',
+            'My project :',
+            '- Mobile Apps: kartusaku, yometu.',
+            '- Web Development: kemnaker (https://skkni.kemnaker.go.id/), yometu (https://yometu.com/), kartusaku(https://www.kartusaku.com/) and cms lps(lempaga penjamin simpanan).',
+            '- Restfull Api: kartusaku, whatsapp cerchat and yometu.',
+            '',
+            'Tech Stacks :',
+            '- PHP Laravel Framework, Golang, Node JS and typescript for developing backend.',
+            '- Reactjs, angular and nextjs for developing frontend.',
+            '- Reactjs, angular and nextjs for developing frontend.',
+            '- Versioning Control : Gitlab, github',
+            '- Database: Mysql, postgresql and mongodb',
+            '- Memory Cached : Redis',
+            '- Containerization : Docker',
+            '- Message Broker: rabbitmq',
+            '- Websocket: socketio'
         ]
     ]
+
+//     [
+//         'PT Global Digital Niaga(Blibli.com)',
+//         '2015 - 2020',
+//         'Leader Fulfillment',
+//         [
+//             'Setting , mereview dan memonitoring KPI setiap hari pada aspek untuk area fulfillment',
+//             'menghandle operasional harian untuk area fulfillment dengan rata rata total order 4000 dan sampai dengan 9000 pcs.',
+//             'Melakukan Koordinasi dengan pihak logistik 3PL dan memastikan order customer sampai dengan tepat waktu sesuai dengan standard waktu yang ditentukan.',
+//             'Membuat Jadwal unuk karyawan',
+//             'Mengasawasi 10-20 Karyawan',
+//             'Melakukan investigasi terhadap pesanan customer apabila ada indikasi kesalahan',
+//             'Memiliki pengetahuan terhadap Warehouse inbound, inventory control dan outbound serta fulfillment.',
+//             'Memastikan berjalannya pekerjaan sesuai dengan SOP'
+//         ]
+//     ]
 
 ];
